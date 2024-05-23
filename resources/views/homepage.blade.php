b4791ff293f573525ab0857ab5b6594b243c97bd
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2024-05-23 18:34:51 +0700

    paddings

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,7 +10,7 @@
   <div id="app">
     <Navbar></Navbar>
     <Hero></Hero>
-    <div class="text-center max-w-md mx-auto">
+    <div class="text-center max-w-md mx-auto py-16">
       <h1 class="text-4xl font-thin pb-3">This is SleeveUp!</h1>
       <p>Leverage your potential and find internships and job positions that suit you.</p>
     </div>
