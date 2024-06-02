b1f244ad8e23b09e6c7bcbb03f4d0f1eb488e813
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2024-06-02 16:09:08 +0700

    fix ngrok

--- resources/js/partials/applicant/Certificate.vue
+++ resources/js/partials/applicant/Certificate.vue
@@ -34,10 +34,18 @@
               <p class="text-[381D4F] font-semibold mr-1">Skills:</p>
               <p class="text-[381D4F] text-wrap break-words whitespace-pre-wrap"> {{ certificate.skills }}</p>
             </div>
-            <div class="flex mb-2">
+            <div class="flex mb-4">
               <p class="text-[381D4F] font-semibold mr-1">Tools:</p>
               <p class="text-[381D4F] text-wrap break-words whitespace-pre-wrap"> {{ certificate.tools }}</p>
             </div>
+            <a :href="certificate.link">
+              <div class="flex text-[5742F5] gap-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                  <path fill-rule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z" clip-rule="evenodd" />
+                </svg>
+                <p class="font-light">Link Sertifikat</p>
+              </div>
+            </a>
           </div>
 
           <!-- Buttons -->
